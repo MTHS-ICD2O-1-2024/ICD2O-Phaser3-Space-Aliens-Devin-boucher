d9ca5eea5f4c60980d44f349b4dd837875cb95c4
Devin-boucher <devin.boucher@stu.ocsb.ca>
2025-04-11 17:19:20 +0000

    fixed formating in js

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@
 // This file contains the JS functions for index.html
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: true,
-        },
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
-    // set background color
-    backgroundColor: 0x5f6e7a,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        // center it in the middle of the page
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
+  },
+// set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // center it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
