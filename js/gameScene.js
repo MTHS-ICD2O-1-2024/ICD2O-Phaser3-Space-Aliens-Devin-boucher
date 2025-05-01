7c006a225c98566e8d436e19a547c925c8919773
default <devin.boucher@stu.ocsb.ca>
2025-05-01 17:06:07 +0000

    fixed gamescene

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,49 +10,49 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
-/**
- * This method is the construtor.
- */
-constructor() {
-  super({ key: "gameScene" })
+  /**
+   * This method is the constructor.
+   */
+  constructor() {
+    super({ key: "gameScene" });
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   * before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets.
+   */
+  preload() {
+    console.log("Game Scene");
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
+    // pass
+  }
+
+  /**
+   * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is running.
+   * @param {number} time - The current time.
+   * @param {number} delta - The delta time in ms since the last frame.
+   */
+  update(time, delta) {
+    // pass
+  }
 }
 
-/**
- * Can be defined on your own Scenes.
- * This method is called by the Scene Manager when the scene starts,
- * before preload() and create().
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
- */
-init(data) {
-  this.cameras.main.setBackgroundColor("#ffffff")
-}
-
-/**
- * Can be defined on your own Scenes.
- * Use it to load assets.
- */
-preload() {
-  console.log("Game Scene")
-}
-
-/**
- * Can be defined on your own Scenes.
- * Use it to create your game objects.
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
- */
-create(data) {
-  // pass
-}
-
-/**
- * Should be overridden by your own Scenes.
- * This method is called once per game step while the scene is running.
- * @param {number} time - The current time.
- * @param {number} delta - The delta time in ms since the last frame.
- */
-update(time, delta) {
-  // pass
-}
-}
-
-export default GameScene
+export default GameScene;
